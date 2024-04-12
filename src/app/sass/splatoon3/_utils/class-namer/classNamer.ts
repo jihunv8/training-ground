9dc6da297a1b30d9e0e396d3733e21de73bb3115
jihun <jihunv8@gmail.com>
2024-04-12 12:30:18 +0900

    Refactor: utils > class-namer를 좀 더 유연하게 변경
    이제 기존 사용법( namer('name1', 'name2') )에 추가로 namer('name1 name2')처럼 사용할 수 있음

--- src/app/sass/splatoon3/_utils/class-namer/classNamer.ts
+++ src/app/sass/splatoon3/_utils/class-namer/classNamer.ts
@@ -1,22 +1,48 @@
-/**
- * @param style scss 모듈
- * 반환된 함수를 통해 class이름을 생성할 수 있습니다.
- * 예시:
- * import style from './index.module.scss';
- * const namer = createClassNamer(style);
- *
- * let isActive;
- * // isActive ? 'wrapper viwer active' : 'wrapper viwer' 반환
- * const className = namer('wrapper', 'viewer', isActive && 'active');
- */
+/** SCSS 모듈 타입 */
+type Style = { readonly [key: string]: string };
 
-export const createClassNamer = (style: { readonly [key: string]: string }) => {
+const splitBySpace = (str: string) => {
+  const splitted = str.trim().split(' ');
+  return splitted;
+};
+
+/**
+ * 주어진 문자열로 class 이름을 생성하는 함수를 반환합니다.
+ * ```tsx
+ *  //예시
+ *  import style from './index.module.scss';
+ *  const namer = createClassNamer(style);
+ *
+ *  function FooComponent({ active }: { active: boolean}) {
+ *    // 아래 3개 div의 className = active ? 'wrapper viwer active' : 'wrapper viwer'
+ *    return (
+ *      <>
+ *        <div className={ namer('wrapper', 'viewer', isActive && 'active') }></div>
+ *        <div className={ namer('wrapper viewer', isActive && 'active') }></div>
+ *        <div className={ namer(`wrapper viewer ${isActive ? 'active' : ''}`)}></div>
+ *      </>
+ *    )
+ *  }
+ *
+ * ```
+ */
+export const createClassNamer = (style: Style) => {
+  const mapToClassName = (names: string[]) => {
+    return names.map((name) => style[name]).filter((className) => className !== undefined);
+  };
+
   return (...names: (string | boolean)[]) => {
-    return names
-      .map((name) => {
-        if (typeof name === 'boolean') return '';
-        return style[name];
-      })
+    const result = names
+      .reduce<string[]>((arr, name) => {
+        if (typeof name === 'boolean') return arr;
+
+        const splittedNames = splitBySpace(name);
+        const classNames = mapToClassName(splittedNames);
+
+        return [...arr, ...classNames];
+      }, [])
       .join(' ');
+
+    return result;
   };
 };
