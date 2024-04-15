b18bc15d41bf3fdcfb4d3dde9ad6c7f9f70ab3ea
jihun <jihunv8@gmail.com>
2024-04-15 20:16:51 +0900

    Add: AreaBattle섹션 반응형 추가

--- src/app/sass/splatoon3/_components/AreaBattle/index.tsx
+++ src/app/sass/splatoon3/_components/AreaBattle/index.tsx
@@ -1,56 +1,74 @@
 'use client';
 
-import Image from 'next/image';
 import style from './index.module.scss';
 
-import frameImage from '@sass/splatoon3/_images/battle/frame.png';
 import { useVisible } from '@/app/sass/splatoon3/_hooks/useVisible';
+import { createClassNamer } from '@/app/sass/splatoon3/_utils/class-namer/classNamer';
+
+const namer = createClassNamer(style);
 
 export default function AreaBattle() {
-  const [twoFormsImg, title, text, areaBattleMovie, ...inkStains] = useVisible(10, { threshold: 0.3 });
+  const [character1Visible, character2Visible, titleVisible, textVisible, movieVisible] = useVisible(5, {
+    threshold: 0.3,
+  });
+
+  const numOfInkStains = 6;
+
+  const inkStainsVisible = useVisible(numOfInkStains, { threshold: 0.5 });
 
   return (
-    <section className={style.wrapper}>
+    <section className={namer('wrapper')}>
       <div
-        ref={twoFormsImg.ref}
-        data-visible-id={twoFormsImg.id}
-        className={`${style.twoFormsImg} ${twoFormsImg.isVisible ? style.visible : ''}`}
+        ref={character1Visible.ref}
+        data-visible-id={character1Visible.id}
+        className={namer('character1', character1Visible.isVisible && 'visible')}
       ></div>
       <h2
-        ref={title.ref}
-        data-visible-id={title.id}
-        className={`${style.title} ${title.isVisible ? style.visible : ''}`}
+        ref={titleVisible.ref}
+        data-visible-id={titleVisible.id}
+        className={namer('title', titleVisible.isVisible && 'visible')}
       >
         영역 배틀
       </h2>
-      <p ref={text.ref} data-visible-id={text.id} className={`${style.text} ${text.isVisible ? style.visible : ''}`}>
-        4명씩 팀을 맺어 지면에 자신의 팀과 같은 색의
-        <br />
-        잉크를 얼마나 칠했는지, 그 면적(영역)으로 경쟁합니다.
+      <p
+        ref={textVisible.ref}
+        data-visible-id={textVisible.id}
+        className={namer('text', textVisible.isVisible && 'visible')}
+      >
+        4명씩 팀을 맺어
+        <br className={namer('onMobile')} />
+        지면에 자신의 팀과 같은 색의
+        <br className={namer('onPC')} />
+        잉크를 얼마나
+        <br className={namer('onMobile')} />
+        칠했는지, 그 면적(영역)으로 경쟁합니다.
         <br />
         그것이 바로 「영역 배틀」!
       </p>
-      <div
-        ref={areaBattleMovie.ref}
-        data-visible-id={areaBattleMovie.id}
-        className={`${style.areaBattleMovie} ${areaBattleMovie.isVisible ? style.visible : ''}`}
-      >
-        <Image className={style.frame} src={frameImage} alt="frame" />
-        <video
-          className={style.movie}
-          src="/assets/sass/splatoon3/videos/battle/battle-area-battle.mp4"
-          preload="none"
-          autoPlay
-          muted
-          loop
-        ></video>
+      <div ref={movieVisible.ref} data-visible-id={movieVisible.id} className={namer('movie')}>
+        <div className={namer('movieInner', movieVisible.isVisible && 'visible')}>
+          <div className={namer('frame')}></div>
+          <video
+            src="/assets/sass/splatoon3/videos/battle/battle-area-battle.mp4"
+            preload="none"
+            autoPlay
+            muted
+            loop
+          ></video>
+        </div>
       </div>
-      {new Array(6).fill(undefined).map((_, i) => (
+      <div
+        ref={character2Visible.ref}
+        data-visible-id={character2Visible.id}
+        className={namer('character2', character2Visible.isVisible && 'visible')}
+      ></div>
+
+      {new Array(numOfInkStains).fill(undefined).map((_, i) => (
         <div
           key={i}
-          ref={inkStains[i].ref}
-          data-visible-id={inkStains[i].id}
-          className={`${style.inkStain} ${style[`stain${i}`]} ${inkStains[i].isVisible ? style.visible : ''}`}
+          ref={inkStainsVisible[i].ref}
+          data-visible-id={inkStainsVisible[i].id}
+          className={namer('inkStain', `ink${i + 1}`, inkStainsVisible[i].isVisible && 'visible')}
         ></div>
       ))}
     </section>
