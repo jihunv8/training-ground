246bd67b2269440bf908f67c957c0d30df49be89
jihun <jihunv8@gmail.com>
2024-04-15 18:36:37 +0900

    Add: BattleWithTwoForms섹션 반응형 추가

--- src/app/sass/splatoon3/_components/BattleWithTwoForms/index.tsx
+++ src/app/sass/splatoon3/_components/BattleWithTwoForms/index.tsx
@@ -1,34 +1,21 @@
-import { useIntersectionObserver } from '@/app/sass/splatoon3/_hooks/useIntersectionObserver';
 import style from './index.module.scss';
-import { useState } from 'react';
+import { createClassNamer } from '@/app/sass/splatoon3/_utils/class-namer/classNamer';
+import { useVisible } from '@/app/sass/splatoon3/_hooks/useVisible';
+
+const namer = createClassNamer(style);
+
 export default function BattleWithTwoForms() {
-  const initVisibleTable = { title: false, text: false, moreDetailBtn: false };
+  const [titleVisible] = useVisible(1, { threshold: 0.7 });
 
-  const [visibleTable, setVisibleTable] = useState(initVisibleTable);
+  const [textVisible] = useVisible(1, { threshold: 0.8 });
 
-  const createVisibleHandler = (name: string): IntersectionObserverCallback => {
-    return ([entry], observer) => {
-      if (entry.isIntersecting) {
-        setVisibleTable((prev) => {
-          const settedVisibleTable = { ...prev, [name]: true };
-          return settedVisibleTable;
-        });
-        observer.disconnect();
-      }
-    };
-  };
-
-  const titleRef = useIntersectionObserver<HTMLHeadingElement>(createVisibleHandler('title'), { threshold: 0.7 });
-
-  const textRef = useIntersectionObserver<HTMLParagraphElement>(createVisibleHandler('text'), { threshold: 0.8 });
-
-  const moreDetailBtnRef = useIntersectionObserver<HTMLButtonElement>(createVisibleHandler('moreDetailBtn'), {
+  const [moreDetailBtnVisible] = useVisible(1, {
     threshold: 1,
   });
 
   return (
-    <section className={style.wrapper}>
-      <div className={style.movieBg}>
+    <section className={namer('wrapper')}>
+      <div className={namer('movieBg')}>
         <video
           src="/assets/sass/splatoon3/videos/battle/battle-with-two-forms.mp4"
           preload="none"
@@ -37,24 +24,29 @@ export default function BattleWithTwoForms() {
           loop
         ></video>
       </div>
-      <div className={style.bg}></div>
+      <div className={namer('bg')}></div>
 
-      <h2 ref={titleRef} className={style.title}>
+      <h2 ref={titleVisible.ref} data-visible-id={titleVisible.id} className={namer('title')}>
         주인공은 오징어!?
-        <div className={`${style.banner} ${visibleTable.title ? style.visible : ''}`}></div>
+        <div className={namer('banner', titleVisible.isVisible && 'visible')}></div>
       </h2>
 
-      <p ref={textRef} className={`${style.text} ${visibleTable.text ? style.visible : ''}`}>
-        플레이어(주인공)는 두 가지 모습을 지닌 신기한 오징어!
-        <br />
-        「인간」 모습으로 잉크를 마음대로 칠하고
-        <br />
-        「오징어」 모습으로 잉크 속을 자유롭게 이동합니다.
+      <p ref={textVisible.ref} data-visible-id={textVisible.id} className={namer('text')}>
+        <span className={namer('textInner', textVisible.isVisible && 'visible')}>
+          플레이어(주인공)는
+          <br className={namer('brOnMobile')} />
+          두 가지 모습을 지닌 신기한 오징어!
+          <br />
+          「인간」 모습으로 잉크를 마음대로 칠하고
+          <br />
+          「오징어」 모습으로 잉크 속을 자유롭게 이동합니다.
+        </span>
       </p>
 
       <button
-        ref={moreDetailBtnRef}
-        className={`${style.moreDetailBtn} ${visibleTable.moreDetailBtn ? style.visible : ''}`}
+        ref={moreDetailBtnVisible.ref}
+        data-visible-id={moreDetailBtnVisible.id}
+        className={namer('moreDetailBtn', moreDetailBtnVisible.isVisible && 'visible')}
       >
         두 모습의 차이점 더 자세히 알아보기
       </button>
