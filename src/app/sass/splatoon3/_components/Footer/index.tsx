5aba12c6a0b404d6b41150c3f61e0e25b1ede32c
jihun <jihunv8@gmail.com>
2024-04-17 11:54:09 +0900

    Remove: 더 이상 사용하지 않는 이미지 삭제

--- src/app/sass/splatoon3/_components/Footer/index.tsx
+++ src/app/sass/splatoon3/_components/Footer/index.tsx
@@ -1,34 +1,17 @@
 import Link from 'next/link';
 import style from './index.module.scss';
 import { createClassNamer } from '@/app/sass/splatoon3/_utils/class-namer/classNamer';
-import Image from 'next/image';
-
-import logoImage from '@sass/splatoon3/_images/footer/logo.png';
-import textBattleImage from '@sass/splatoon3/_images/footer/text-battle.png';
-import textBattleOnImage from '@sass/splatoon3/_images/footer/text-battle-on.png';
-import textCustomizeImage from '@sass/splatoon3/_images/footer/text-customize.png';
-import textCustomizeOnImage from '@sass/splatoon3/_images/footer/text-customize-on.png';
-import textWorldImage from '@sass/splatoon3/_images/footer/text-world.png';
-import textWorldOnImage from '@sass/splatoon3/_images/footer/text-world-on.png';
-import textHeromodeImage from '@sass/splatoon3/_images/footer/text-heromode.png';
-import textHeromodeOnImage from '@sass/splatoon3/_images/footer/text-heromode-on.png';
-import textCoopImage from '@sass/splatoon3/_images/footer/text-coop.png';
-import textCoopOnImage from '@sass/splatoon3/_images/footer/text-coop-on.png';
-import textFesImage from '@sass/splatoon3/_images/footer/text-fes.png';
-import textFesOnImage from '@sass/splatoon3/_images/footer/text-fes-on.png';
-import textMovieImage from '@sass/splatoon3/_images/footer/text-movie.png';
-import textMovieOnImage from '@sass/splatoon3/_images/footer/text-movie-on.png';
 
 const namer = createClassNamer(style);
 
 const menuItemInfos = [
-  { name: 'Battle', href: 'temp', img: textBattleImage, imgOn: textBattleOnImage },
-  { name: 'Customize', href: 'temp', img: textCustomizeImage, imgOn: textCustomizeOnImage },
-  { name: 'World', href: 'temp', img: textWorldImage, imgOn: textWorldOnImage },
-  { name: 'Heromode', href: 'temp', img: textHeromodeImage, imgOn: textHeromodeOnImage },
-  { name: 'Coop', href: 'temp', img: textCoopImage, imgOn: textCoopOnImage },
-  { name: 'Fes', href: 'temp', img: textFesImage, imgOn: textFesOnImage },
-  { name: 'Movie', href: 'temp', img: textMovieImage, imgOn: textMovieOnImage },
+  { name: 'Battle', href: 'temp' },
+  { name: 'Customize', href: 'temp' },
+  { name: 'World', href: 'temp' },
+  { name: 'Heromode', href: 'temp' },
+  { name: 'Coop', href: 'temp' },
+  { name: 'Fes', href: 'temp' },
+  { name: 'Movie', href: 'temp' },
 ];
 
 export default function Footer() {
